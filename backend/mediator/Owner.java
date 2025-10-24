052589ff4d593ace03c7f98dd72bca2ae02a6988
Himanshu Sinha <himanshuthegreat456@gmail.com>
2025-10-24 16:30:16 +0530

    Added Owner

---
+++ backend/mediator/Owner.java
@@ -0,0 +1,12 @@
+package mediator;
+
+public class Owner extends User {
+    public Owner(Mediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    public void claimItem(String itemName) {
+        System.out.println(name + " is trying to claim item: " + itemName);
+        mediator.notify("Claim", "Claim request for: " + itemName);
+    }
+}
